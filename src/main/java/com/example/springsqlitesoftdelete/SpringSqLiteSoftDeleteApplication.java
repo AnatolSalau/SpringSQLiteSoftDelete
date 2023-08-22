11c05d55d372d5adb0f5592702e56aeec9be050f
AnatolSalau <salovanatoly5@mail.ru>
2023-08-22 14:51:43 +0300

    Init commit

---
+++ src/main/java/com/example/springsqlitesoftdelete/SpringSqLiteSoftDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsqlitesoftdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqLiteSoftDeleteApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SpringSqLiteSoftDeleteApplication.class, args);
+      }
+
+}
---
+++ src/test/java/com/example/springsqlitesoftdelete/SpringSqLiteSoftDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsqlitesoftdelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqLiteSoftDeleteApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
